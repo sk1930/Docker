2c4c211fb46d9da37f50b841f41f5e4926da1f7e
Ubuntu <ubuntu@ip-172-31-19-7.ec2.internal>
2024-05-19 23:59:28 +0000

    added demo

--- examples/002webAppExample/devops/devops/settings.py
+++ examples/002webAppExample/devops/devops/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-49hsrt24v$1b@+#c4$2vm=g#s+w29c%x@w)l4mmnyy0k2m+l_1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
